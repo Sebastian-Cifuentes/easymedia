956e998a84a06de40d051f062d45f4ceb665bda6
Sebastian Cifuentes <sebastianc420@gmail.com>
2023-12-11 18:15:30 -0500

    add docker compose and swagger library

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Easymedia RestfulAPI')
+    .setDescription('Easymedia endpoind')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('ap', app, document);
+
   await app.listen(3000);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
